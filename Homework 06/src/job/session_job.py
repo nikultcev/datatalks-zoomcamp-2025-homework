7e51c378a155bf1b3a3c9c5279395912ecb0d9cc
nikultcev <122282119+nikultcev@users.noreply.github.com>
2025-03-22 17:49:12 +0400

    Add files via upload

---
+++ Homework 06/src/job/session_job.py
@@ -0,0 +1,106 @@
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.table import EnvironmentSettings, DataTypes, TableEnvironment, StreamTableEnvironment
+from pyflink.common.watermark_strategy import WatermarkStrategy
+from pyflink.common.time import Duration
+import time
+
+def create_events_aggregated_sink(t_env):
+    table_name = 'processed_events_aggregated'
+    sink_ddl = f"""
+        CREATE TABLE {table_name} (
+            session_start TIMESTAMP(3),
+            session_end TIMESTAMP(3),
+            PULocationID int,
+            DOLocationID int,
+            trips_count BIGINT,
+            PRIMARY KEY (session_start, session_end) NOT ENFORCED
+        ) WITH (
+            'connector' = 'jdbc',
+            'url' = 'jdbc:postgresql://postgres:5432/postgres',
+            'table-name' = '{table_name}',
+            'username' = 'postgres',
+            'password' = 'postgres',
+            'driver' = 'org.postgresql.Driver'
+        );
+        """
+    
+    print(sink_ddl)
+    t_env.execute_sql(sink_ddl)
+    return table_name
+
+def create_events_source_kafka(t_env):
+    table_name = "events"
+    source_ddl = f"""
+        CREATE TABLE {table_name} (
+            lpep_pickup_datetime timestamp,
+            lpep_dropoff_datetime timestamp,
+            PULocationID int,
+            DOLocationID int,
+            passenger_count int,
+            trip_distance decimal,
+            tip_amount decimal,
+            event_timestamp BIGINT,
+            event_watermark AS CAST(lpep_dropoff_datetime AS TIMESTAMP(3)),
+            WATERMARK for event_watermark as event_watermark - INTERVAL '5' SECOND
+        ) WITH (
+            'connector' = 'kafka',
+            'properties.bootstrap.servers' = 'redpanda-1:29092',
+            'topic' = 'green-trips',
+            'scan.startup.mode' = 'earliest-offset',
+            'properties.auto.offset.reset' = 'earliest',
+            'format' = 'json'
+        );
+        """
+    t_env.execute_sql(source_ddl)
+    return table_name
+
+
+def log_aggregation():
+    # Set up the execution environment
+    env = StreamExecutionEnvironment.get_execution_environment()
+    env.enable_checkpointing(10 * 1000)
+    env.get_checkpoint_config().set_checkpoint_timeout(60000)
+    env.set_parallelism(1)
+
+    # Set up the table environment
+    settings = EnvironmentSettings.new_instance().in_streaming_mode().build()
+    t_env = StreamTableEnvironment.create(env, environment_settings=settings)
+
+    watermark_strategy = (
+        WatermarkStrategy
+        .for_bounded_out_of_orderness(Duration.of_seconds(5))
+        .with_timestamp_assigner(
+            # This lambda is your timestamp assigner:
+            #   event -> The data record
+            #   timestamp -> The previously assigned (or default) timestamp
+            lambda event, timestamp: event[2]  # We treat the second tuple element as the event-time (ms).
+        )
+    )
+    try:
+        # Create Kafka table
+        source_table = create_events_source_kafka(t_env)
+        aggregated_table = create_events_aggregated_sink(t_env)
+
+        time.sleep(10)
+
+        t_env.execute_sql(f"""
+        INSERT INTO {aggregated_table}
+        SELECT
+            window_start AS session_start,
+            window_end AS session_end,
+            PULocationID,
+            DOLocationID,
+            count(*) as trips_count
+        FROM TABLE(
+            TUMBLE(TABLE {source_table}, DESCRIPTOR(event_watermark), INTERVAL '5' MINUTE)
+        )
+        GROUP BY window_start, window_end,PULocationID,DOLocationID;
+        
+        """).wait()
+
+    except Exception as e:
+        print("Writing records from Kafka to JDBC failed:", str(e))
+
+
+if __name__ == '__main__':
+    log_aggregation()
